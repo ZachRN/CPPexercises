87acbedd86f69c313e2ea9aeea657e4a880a0d93
Zachary Najda <zacharynajda@zacharys-air.home>
2022-10-31 02:30:39 +0100

    small push

---
+++ c05/ex00/Bureaucrat.hpp
@@ -0,0 +1,32 @@
+#ifndef BUREACRAT_HPP
+# define BUREACRAT_HPP
+
+#include <iostream>
+
+class Bureaucrat
+{
+	public:
+		Bureaucrat();
+		Bureaucrat(const Bureaucrat &src);
+		Bureaucrat& operator=(const Bureaucrat &src);
+		~Bureaucrat();
+
+	private:
+		const std::string	name;
+		int	grade;
+	public:
+
+};
+
+class Exception
+{
+	public:
+		Exception(std::string msg);
+		~Exception();
+	private:
+		std::string	error;
+
+
+};
+
+#endif
